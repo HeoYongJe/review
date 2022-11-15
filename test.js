15df05b5c311ca20f7b8800a5d2ea2110b4952e5
HeoYongJe <hyj.campus@gmail.com>
2022-11-15 23:23:34 +0900

    간략한

--- test.js
+++ test.js
@@ -4,3 +4,27 @@ console.log(result);
 const arr = ['a','b','c'];
 console.log(arr);
 arr[0];
+
+// alert('Hello World');
+//         let fruite = ['apple','grape']
+//         console.log(fruite);
+
+
+// alert("에러가 발생합니다.")
+// [1, 2].forEach(alert)
+
+alert("제대로 동작합니다.");
+[1, 2].forEach(alert)
+
+let message;
+message = 'hello';
+message = 'hello!!';
+
+alert(message);
+
+const birthday;
+birthday = '890222';
+birthday = '19890222';
+// Uncaught SyntaxError: Missing initializer in const declaration 상수 재할당 x
+alert(birthday);
+
