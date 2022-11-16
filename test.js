d53d70e323b919b8166232a9e890ad566d141bd8
HeoYongJe <hyj.campus@gmail.com>
2022-11-16 23:52:04 +0900

    복습

--- test.js
+++ test.js
@@ -16,15 +16,23 @@ arr[0];
 alert("제대로 동작합니다.");
 [1, 2].forEach(alert)
 
-let message;
-message = 'hello';
+let message = 'hello';
 message = 'hello!!';
 
 alert(message);
 
-const birthday;
-birthday = '890222';
-birthday = '19890222';
-// Uncaught SyntaxError: Missing initializer in const declaration 상수 재할당 x
+const birthday = '890222';
+// birthday = '19890222';
+// Uncaught SyntaxError: Missing initializer in const declaration 상수 재할당 x 
 alert(birthday);
+console.log(birthday);
 
+
+let n = 123;
+n = 12.222;
+console.log(n);
+
+
+let name = "heo";
+console.log(`hello, ${name}`);
+// "" , '' 상관x  / ``역따옴표는 위처럼 변수나 표현식을 감싼후 ${}안에 넣어서 사용가능
