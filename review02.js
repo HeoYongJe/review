c7e405dbb8fb2e1a5d3840c810f388371cf113c5
HeoYongJe <hyj.campus@gmail.com>
2022-11-19 03:23:37 +0900

    논리연산자

--- review02.js
+++ review02.js
@@ -18,3 +18,7 @@ console.log(result3);
 
 
 
+//논리연산자
+alert( null || 2 || undefined ); //2가출력
+alert( 1 && null && 2 ); //null 출력 
+alert( null || 2 && 3 || 4 ); // && 가 || 보다 우선순위가 높으므로 먼저 실행, 이후 2 && 3 = 3 이므로 3이 출력
