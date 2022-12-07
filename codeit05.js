24d3f2fc9e26e84b9c5c5fd6988745fa673af035
HeoYongJe <hyj.campus@gmail.com>
2022-12-07 22:36:00 +0900

    코드잇-변수

---
+++ codeit05.js
@@ -0,0 +1,12 @@
+let espressoPrice ; //선언
+espressoPrice = 3000; // 할당
+console.log(espressoPrice) //3000
+
+
+let lattePrice = 4300;
+let mochaPrice = 5000;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 2);
+console.log(mochaPrice * 2 + lattePrice * 5)
+
