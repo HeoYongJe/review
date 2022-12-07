b3bf486e40f1f1040ff181c08be51c08a4b5f3fd
HeoYongJe <hyj.campus@gmail.com>
2022-12-07 23:57:43 +0900

    코드잇-변수실습

---
+++ codeitPractice05.js
@@ -0,0 +1,10 @@
+let espresso = 10;
+let milk = 170;
+let chocolateSyrup = 50;
+let whippedCream = 60;
+
+console.log(espresso) //10
+console.log(espresso + milk) // 180
+console.log(espresso +chocolateSyrup + milk) // 230
+console.log(espresso + chocolateSyrup + milk + whippedCream) //290
+
