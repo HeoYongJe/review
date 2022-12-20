22d17e82f87e0f6db9c1d70bf2388d3a8a78af8d
HeoYongJe <hyj.campus@gmail.com>
2022-12-20 23:26:52 +0900

    코드잇 - while 예시

--- core3_13_while.js
+++ core3_13_while.js
@@ -10,3 +10,19 @@ while (i % 7 !== 0) {
 }
 
 console.log(y);
+
+
+//while문을 활용해서 정수 180의 약수를 모두 출력하고, 총 몇 개의 약수가 있는지 출력
+
+const N = 180;
+let x = 1;
+let count = 0;
+while(x <= N){
+  if(N % x === 0){
+    console.log(x);
+    count ++;
+  }
+  x += 1;
+}
+
+console.log(`${N}의 약수는 총 ${count}개입니다.`)
