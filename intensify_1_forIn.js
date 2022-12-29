c5efd15d5ec01bbf131c66a9ecbafd21ce5a8f0e
HeoYongJe <hyj.campus@gmail.com>
2022-12-29 23:50:35 +0900

    코드잇-for..in 예문 추가

--- intensify_1_forIn.js
+++ intensify_1_forIn.js
@@ -10,3 +10,26 @@ for (let k in codeit) {
   console.log(k);
   console.log(codeit[k]);
 }
+
+let yongjeScore = {
+	'html': 70,
+	'css': 70,
+	'js': 10,
+};
+
+function pass(object) {
+
+	let totalAdd = 0;
+	for (let key in object) {
+	 // console.log(key);
+	 // console.log(object[key]);
+    totalAdd += object[key];
+	}
+	if (totalAdd > 70) {
+	  console.log('축하합니다! 합격입니다!');
+	} else {
+	  console.log('아쉽지만 불합격입니다..');
+	}
+}
+
+pass(yongjeScore);
