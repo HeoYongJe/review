19fae7ef6e7eeeca471ed43a489df1932eee1f02
HeoYongJe <hyj.campus@gmail.com>
2023-01-09 22:43:23 +0900

    강의복습- 변수

--- codeit05.js
+++ codeit05.js
@@ -1,6 +1,9 @@
 let espressoPrice ; //선언
 espressoPrice = 3000; // 할당
+// let espressPrice = 3000
+
 console.log(espressoPrice) //3000
+
 
 
 let lattePrice = 4300;
@@ -9,4 +12,23 @@ let mochaPrice = 5000;
 console.log(espressoPrice * 2);
 console.log(lattePrice * 2);
 console.log(mochaPrice * 2 + lattePrice * 5)
+
+//Kcal
+
+let espressoKal = 10;
+let milk = 170;
+let chocolteSyrup = 50;
+let whippedCream = 60;
+
+//에스프레소
+console.log(espressoKal);
+
+//라떼 에스프레소 + 우유
+console.log(espressoKal + milk);
+
+//초코라떼
+console.log(espressoKal + milk + chocolteSyrup);
+
+//초코라떼 휘핑추가
+console.log(espressoKal + milk + chocolteSyrup + whippedCream);
 
