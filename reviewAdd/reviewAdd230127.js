7d85de5af6b7a138d1867e9e926fced096559c5c
HeoYongJe <hyj.campus@gmail.com>
2023-01-29 23:22:14 +0900

    코드잇 복습 - date 예제 추가

--- reviewAdd/reviewAdd230127.js
+++ reviewAdd/reviewAdd230127.js
@@ -6,3 +6,16 @@ let timeDiff = myDate.getTime() - today.getTime();
 let dDay = Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + ' day';
 console.log(dDay);
 
+
+
+// 공부시작한날 230101
+let today1 = new Date(2023,01,29);
+let studyStart = new Date(2023,01,01);
+
+function studyDay(study) {
+  let timeDiff1 = today1.getTime() - studyStart.getTime();
+  let day = Math.floor( timeDiff1 / 1000 / 1000 / 24); 
+  console.log(`오늘은 공부한지 ${day + 1} 일 되는 날입니다.`)
+}
+
+studyDay(studyStart);
