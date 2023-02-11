ca013e8c279c92fd7606d8a2c0d2ddf521af2a66
HeoYongJe <hyj.campus@gmail.com>
2023-02-11 23:02:50 +0900

    코드잇 복습- 문자열과 배열은 다르다

---
+++ reviewAdd/reviewAdd230211.js
@@ -0,0 +1,12 @@
+//문자열과 배열
+//배열은 바뀔수있는 자료형 / 문자열은 바뀔수 없는 자료형
+
+let myArray = ['H','E','O'];
+myArray[0] = 'A';
+console.log(myArray); //'A','E','O'
+
+// 문자열은 immutable
+let myString = 'HeoYongJe';
+myString[0] = 'B';
+console.log(myString); //HeoYongJe
+
