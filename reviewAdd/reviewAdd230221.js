5d914c82d62c2d424ffad9a88a21b45971f95847
HeoYongJe <hyj.campus@gmail.com>
2023-02-25 23:52:44 +0900

    코드잇 - 거스름돈 예시 완료

--- reviewAdd/reviewAdd230221.js
+++ reviewAdd/reviewAdd230221.js
@@ -5,7 +5,20 @@ function calculateChange(payment, cost) {
   const fiftyCount = (change - (change % 50000)) / 50000;
   change = change - 50000 * fiftyCount;
 
+  const tenThCount = (change - (change % 10000)) / 10000;
+  change = change - 10000 * tenThCount;
+  
+  const fiveThCount = (change - (change % 5000)) / 5000;
+  change = change - 5000 * fiveThCount;
+  
+  const thousandCount = (change - (change % 1000)) / 1000;
+  change = change - 1000 * thousandCount;
+
+
   console.log(`50000원 지폐 : ${fiftyCount}장`);
+  console.log(`10000원 지폐 : ${tenThCount}장`);
+  console.log(`5000원 지폐 : ${fiveThCount}장`);
+  console.log(`1000원 지폐 : ${thousandCount}장`);
 
 }
 
