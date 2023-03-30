f3eb500cab766a36c9da0d8032c3d8e61ed1705a
HeoYongJe <hyj.campus@gmail.com>
2023-03-31 03:11:44 +0900

    코드잇

--- modernJS/230329.js
+++ modernJS/230329.js
@@ -2,8 +2,10 @@
 const numbers  = [1,2,3,4,5];
 
 //some 조건을 만족하는 요소가 1개 이상 있는지
-const someReturm = numbers.some((el) => el > 5);  
+const someReturn = numbers.some((el) => el > 5);  
 
 //every 모든 요소가 조건을 만족하는지
 const everyReturn = numbers.every((el) => el > 5);
 
+console.log('some:', someReturn);
+console.log('every:', everyReturn);
