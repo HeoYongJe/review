d5b977b2ff7d854ba96dc1349abac668a16d5fe9
HeoYongJe <hyj.campus@gmail.com>
2023-04-02 22:29:44 +0900

    코드잇 - 이벤트와 버튼클릭

--- interactiveJS/js/index0331_01.js
+++ interactiveJS/js/index0331_01.js
@@ -33,3 +33,14 @@ for (let btn of colorBtns) {
 // length 프로퍼티가 있다.
 // 배열의 기본 메소드를 사용할 수 없다.
 // Array.isArray(유사배열)은 false다.
+
+
+//css 선택자로 태그 선택하기
+const myTag = document.querySelector('.color-btn');
+console.log(myTag);
+// color-btn 선택자중 맨 첫번째께 나옴
+
+
+const myTags = document.querySelectorAll('.color-btn');
+console.log(myTags);
+// 여러요소를 선택하고싶을때 / All을 사용 할 경우 NodeList라는 유사배열이 나오며, 해당 클래스를 다 선택할 수 있다
